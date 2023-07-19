eb452dba422e695557567553c952dd893a6511c3
Saurabh <saurabh.sb83@gmail.com>
2023-07-19 09:33:58 +0530

    added bytes.NewBuffferString

--- mystrings.go
+++ mystrings.go
@@ -1,5 +1,10 @@
 package mystrings
 
+import (
+	"bytes"
+	"fmt"
+)
+
 func Reverse(s string) string {
 
 	result := ""
@@ -8,5 +13,18 @@ func Reverse(s string) string {
 		result = string(v) + result
 	}
 	return result
+
+}
+
+func Reversev2(s string) {
+
+	result := ""
+	for _, v := range s {
+
+		result = string(v) + result
+	}
+
+	buf := bytes.NewBufferString(result)
+	fmt.Fprintln(buf)
 
 }
